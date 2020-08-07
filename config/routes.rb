8a68605fa5c69ed946b956d42674182c269d5513
mihooshima <meishui1107@gmail.com>
2020-08-07 15:40:30 +0900

    サイバーのフロントを実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get 'message/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # こちらは削除してOKです
-  root "messages#index"
+  root  "messages#index"
 end
